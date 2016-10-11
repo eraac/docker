541e30982334ee432d899bf50f1c26aa9b458b67
Eraac <kevin@labesse.me>
2016-10-12 00:11:05 +0200

    add ifttt_enable

--- templates/symfony/bin/deploy.sh
+++ templates/symfony/bin/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 # == CONFIG ==
+ifttt_enable=false
 ifttt_event='xxx'
 ifttt_key='xxx'
 # ====
 
-cd "$(dirname "$0")/.."
+#cd "$(dirname "$0")/.."
 
-docker-compose pull
-docker-compose down
-docker-compose up -d
+#docker-compose pull
+#docker-compose down
+#docker-compose up -d
 
-curl --silent -X POST https://maker.ifttt.com/trigger/${ifttt_event}/with/key/${ifttt_key} > /dev/null
+if [ "$ifttt_enable" = true ] ; then
+	curl --silent -X POST https://maker.ifttt.com/trigger/${ifttt_event}/with/key/${ifttt_key} > /dev/null
+fi
+
