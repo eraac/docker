ec63b6a34badff9fb847ca0f78cd365e9b9abe0f
Eraac <kevin@labesse.me>
2016-11-07 13:27:10 +0100

    update permission.sh

--- templates/symfony/bin/permission.sh
+++ templates/symfony/bin/permission.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose exec php sh -c "chmod -R 665 /var/www/symfony/var/logs"
-docker-compose exec php sh -c "chmod -R 665 /var/www/symfony/var/cache"
+docker-compose exec php sh -c "chmod -R 665 /var/www/var/logs"
+docker-compose exec php sh -c "chmod -R 665 /var/www/var/cache"
