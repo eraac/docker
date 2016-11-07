077618a9eb2e17f9d4b5d110c6a7db347af34ed1
Eraac <kevin@labesse.me>
2016-11-07 16:55:18 +0100

    remove useless script

--- templates/symfony/bin/run-docker-hook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# == CONFIG ==
-uuid='xxx' # use uuidgen
-script='sh /path/to/bin/deploy.sh'
-port='8555'
-# ====
-
-nohup docker-hook -t ${uuid} -c ${script} --port ${port} > /dev/null 2>&1 &
